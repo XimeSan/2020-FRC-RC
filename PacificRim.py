9e07c0190dbc4f92c55d7749aaed6c8435ac6821
XimeSan <a01275072@itesm.mx>
2020-01-22 22:30:13 -0600

    Agregue un codigo muy basico para poder partir de aqui

---
+++ PacificRim.py
@@ -0,0 +1,16 @@
+on_and_off_piston = 2
+
+manual_impulsor_on = 3 
+manual_impulsor_off = 4 
+on_and_off_impulsor = 5
+
+up_by_hand = 15 # POV arriba
+down_by_hand = 17 #POV abajo
+
+up_platform_middle_piston = 10
+up_platform_high_piston = 8 
+
+turbo = 1
+accomodate = 11
+
+button_breaker = 7
--- oi.py
+++ oi.py
@@ -1,1 +1,13 @@
-print("hola Isma")
+from state import state
+import wpilib 
+
+def ReadControllerInputs():
+
+	chasis_controller = wpilib.Joystick(0)
+	abilities_controller = wpilib.Joystick(1)
+
+	state["x_axis"] = chasis_controller.getX()
+	state["y_axis"] = chasis_controller.getY()
+	state["z_axis"] = chasis_controller.getRawAxis(4)
+
+
