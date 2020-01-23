4a7d6d6f7f0dbb5eb710772aa37624cbb18abf9f
Isma <a01275072@itesm.mx>
2020-01-22 22:33:20 -0600

    Ajustes a base de codigo

--- robot.py
+++ robot.py
@@ -17,7 +17,6 @@ class MyRobot(wpilib.TimedRobot):
 		self.front_right_motor = wpilib.Talon(2)
 		self.rear_right_motor = wpilib.Talon(3)
 
-
 		self.drive = MecanumDrive(
 			self.front_left_motor,
 			self.rear_left_motor,
@@ -44,7 +43,6 @@ class MyRobot(wpilib.TimedRobot):
 
 		self.drive.driveCartesian(powerX * 0.6,-powerY * 0.6, powerZ * 0.5, 0)
 		
-
 
 if __name__ == '__main__':
 	wpilib.run(MyRobot)
